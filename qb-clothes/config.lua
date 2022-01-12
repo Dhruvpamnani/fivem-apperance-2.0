e9dcbd69cb39803eceba0f23945c4a5461ddcfd3
Enzo <86290612+Enzo-DC@users.noreply.github.com>
2022-01-12 23:18:56 +0100

    update blips

--- qb-clothes/config.lua
+++ qb-clothes/config.lua
@@ -9,114 +9,96 @@ Config.Debug = true
 Config.ClothingShops = {
 	[1] = {
 		blip = true,
-		coords = vector3(72.3, -1399.1, 28.4),
+		coords = vector3(427.0880, -805.900, 28.491),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[2] = {
 		blip = true,
-		coords = vector3(-708.71, -152.13, 36.4),
+		coords = vector3(73.86779, -1392.99, 28.376),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[3] = {
 		blip = true,
-		coords = vector3(-165.15, -302.49, 38.6),
+		coords = vector3(126.6333, -225.045, 53.557),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[4] = {
 		blip = true,
-		coords = vector3(428.7, -800.1, 28.5),
+		coords = vector3(-164.627, -301.921, 38.733),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[5] = {
 		blip = true,
-		coords = vector3(-829.4, -1073.7, 10.3),
+		coords = vector3(-708.208, -152.955, 36.415),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[6] = {
 		blip = true,
-		coords = vector3(-1449.16, -238.35, 48.8),
+		coords = vector3(-1449.71, -238.977, 48.813),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[7] = {
 		blip = true,
-		coords = vector3(11.6, 6514.2, 30.9),
+		coords = vector3(-1193.21, -766.425, 16.316),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[8] = {
 		blip = true,
-		coords = vector3(122.98, -222.27, 53.5),
+		coords = vector3(-823.166, -1072.28, 10.328),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[9] = {
 		blip = true,
-		coords = vector3(1696.3, 4829.3, 41.1),
+		coords = vector3(-1206.36, -1457.99, 3.3487),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[10] = {
 		blip = true,
-		coords = vector3(618.1, 2759.6, 41.1),
+		coords = vector3(-3169.77, 1042.293, 19.863),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[11] = {
 		blip = true,
-		coords = vector3(1190.6, 2713.4, 37.2),
+		coords = vector3(612.9566, 2763.617, 41.088),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[12] = {
 		blip = true,
-		coords = vector3(-1193.4, -772.3, 16.3),
+		coords = vector3(1196.610, 2711.712, 37.222),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[13] = {
 		blip = true,
-		coords = vector3(-3172.5, 1048.1, 19.9),
+		coords = vector3(1695.368, 4823.105, 41.063),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[14] = {
 		blip = true,
-		coords = vector3(-1108.4, 2708.9, 18.1),
+		coords = vector3(5.868691, 6511.372, 30.877),
 		MarkerSize   = {x = 7.0, y = 7.0, z = 1.0}
 	},
 	[15] = {
 		blip = true,
-		coords = vector3(300.60162353516, -597.76068115234, 42.18409576416),
+		coords = vvector3(4495.485, -4452.16, 3.3664),
 		MarkerSize   = {x = 3.0, y = 5.0, z = 0.0}
 	},
 	[16] = {
 		blip = true,
-		coords = vector3(-1622.6466064453, -1034.0192871094, 13.145475387573),
+		coords = vector3(-1102.50, 2711.632, 18.107),
 		MarkerSize   = {x = 2.5, y = 2.0, z = 0.0}
 	},
-	[17] = {
-		blip = true,
-		coords = vector3(1861.1047363281, 3689.2331542969, 34.276859283447),
-		MarkerSize   = {x = 2.0, y = 2.0, z = 0.0}
-	},
-	[18] = {
-		blip = true,
-		coords = vector3(1834.5977783203, 3690.5405273438, 34.270645141602),
-		MarkerSize   = {x = 2.0, y = 2.0, z = 0.0}
-	},
-	[19] = {
-		blip = true,
-		coords = vector3(1742.1407470703, 2481.5856933594, 45.740657806396),
-		MarkerSize   = {x = 4.0, y = 2.0, z = 0.0}
-	},
-	[20] = {
-		blip = true,
-		coords = vector3(105.7079, -1302.983, 28.793018),
-		MarkerSize   = {x = 2.0, y = 2.0, z = 0.0}
-	},	
 }
 
+
 Config.BarberShops = {
-	vector3(-814.3, -183.8, 36.6),
-	vector3(136.8, -1708.4, 28.3),
-	vector3(-1282.6, -1116.8, 6.0),
-	vector3(1931.5, 3729.7, 31.8),
-	vector3(1212.8, -472.9, 65.2),
-	vector3(-34.31, -154.99, 55.8),
-	vector3(-278.1, 6228.5, 30.7),
+	vector3(922.7774, 21.39071, 70.833),
+	vector3(136.8038, -1708.44, 28.291),
+	vector3(-1282.60, -1116.80, 5.9938),
+	vector3(1931.613, 3729.659, 31.844),
+	vector3(1212.713, -472.972, 65.207),
+	vector3(-32.9359, -152.341, 56.076),
+	vector3(-278.205, 6228.501, 30.695),
+	vector3(-814.235, -183.812, 36.568)
 }
 
 Config.ClothingRooms = {
